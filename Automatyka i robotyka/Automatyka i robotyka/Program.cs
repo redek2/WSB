f49fd6e44046867d8208a5ff7d8d88288c9d9de9
redek2 <128227165+redek2@users.noreply.github.com>
2024-11-24 00:00:25 +0100

    Add files via upload

---
+++ Automatyka i robotyka/Automatyka i robotyka/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int steps = 90;
+        double maxPosition = 90;
+        double[] positions = new double[steps + 1];
+
+        for (int k = 0; k <= steps; k++)
+        {
+            double position = maxPosition * Math.Sin((Math.PI / 2) * k / steps);
+            positions[k] = position;
+            position = Math.Round(positions[k],2);
+            Console.WriteLine(position);
+        }
+    }
+}
